19fa5e469cb7ed95face911130d00651c50ca26d
ryan.newton@alum.mit.edu <unknown>
2010-08-01 13:51:01 +0000

    RN134# Some refactoring of ReadHarch.  First steps towards extracting partition structure.

--- Intel/Cnc/Spec/ReadHarch.hs
+++ Intel/Cnc/Spec/ReadHarch.hs
@@ -1,4 +1,4 @@
-
+{-# LANGUAGE RecordWildCards #-}
 
 ----------------------------------------------------------------------------------------------------
 -- Read .harch profiled/partitioned graph files.
@@ -11,9 +11,12 @@ import Text.Parsec
 import Text.Parsec.Char
 import Text.Parsec.Combinator
 import Text.Parsec.String
-import Data.List
 import System.IO
 import Control.Monad
+import Data.List
+import Data.Set as S hiding (map, filter, partition)
+import Data.List.Split (splitOn)
+import Data.Graph
 
 ----------------------------------------------------------------------------------------------------
 
@@ -22,9 +25,24 @@ data HarchNode = HarchNode {
     name       :: String,             -- A mandatory field.
     properties :: [(String, String)], -- 
     num        :: Int,                -- Using numeric idenifiers for now
+    in_edges   :: [Int],
     out_edges  :: [Int]
   }
  deriving Show
+
+
+-- A datatype for Harch trees:
+-- Parameterized by the type of the partitions themselves.
+data HarchTree a = HT a [HarchTree a]
+
+-- A simple representation to start with is a set of Ints for each partition:
+type HarchTreeUnordered = HarchTree (Set Int)
+
+-- After topological sorting the partitions are ordered.
+type HarchTreeOrdered = HarchTree [Int]
+
+----------------------------------------------------------------------------------------------------
+-- Parsec parser for .harch syntax:
 
 harchfile :: Parser [HarchNode]
 harchfile = 
@@ -33,13 +51,34 @@ harchfile =
      return$ map (\ (n,rec) -> rec { num= n })
 	         (zip [1..] nodes)
 
+-- Remove one property from a property list.
+popProp name pls = 
+    case partition ((== name) . fst) pls of
+      ([(_,nm)], rest) -> (nm, rest)
+      (ls,_) -> error$ "Currently, exactly one '"++ show name ++"' property is required for each graph node, not "
+		        ++ show (length ls)++ ": "++ show ls
+
 harchnode :: Parser HarchNode
 harchnode = 
   do whitespc; char '%'; whitespc; string "HARCHNODE"; whitespc
-     ps   <- props;    whitespc; newline; 
-     nums <- numbers;  whitespc; newline;
-     let ([("name",nm)], rest) = partition ((== "name") . fst) ps 
-     return HarchNode { name= nm, properties= rest, out_edges= nums, num=0 }
+     ps   <- props;    newline; 
+     (wght:edges) <- numbers;  whitespc; newline;
+
+     -- Parse out all the special fields:
+     let (nm, rest) = popProp "name" ps 
+
+     -- Are the edges mentioned inbound or outbound?
+     let (dirs,rest2) = popProp "directions" rest
+     let (ins,outs) = 
+	  if not (length dirs == length edges) 
+	  then error$ "Number of 'directions' incorrect, "++ show (length dirs) ++" "++
+		      show dirs ++ " expected "++ show (length edges) ++ " for edges " ++ show edges
+	  else (map snd $ filter ((== '0') . fst) $ zip dirs edges,
+		map snd $ filter ((== '1') . fst) $ zip dirs edges)
+
+     return HarchNode { name= nm, properties= rest2, 
+			in_edges= ins, out_edges= outs, 
+			num=0 }
 
 spc = oneOf " \t"
 whitespc = many spc
@@ -53,14 +92,14 @@ prop = do w <- many1 letter
 
 -- Having problems with sepBy:
 props :: Parser [(String,String)]
-props = prop `sepBy` (many1 spc)
+props = prop `sepEndBy` (many1 spc)
 
 numbers :: Parser [Int]
 numbers = do whitespc
 	     strs <- (many1 digit) `sepEndBy` (many1 spc)
 	     return$ map read strs
 
-----------------------------------------------------------------------------------------------------
+------------------------------------------------------------
 -- Generic harness for running a parser:
 run :: Show a => Parser a -> String -> a
 run p input
@@ -68,11 +107,24 @@ run p input
             Left err -> error ("parse error at "++ show err)
             Right x  -> x
 
+main = testread
 testread = 
- do file <- openFile "/Users/newton/cnc/experimental/graphPartitioner/test.harch" ReadMode 
+-- do file <- openFile "/Users/newton/cnc/experimental/graphPartitioner/test.harch" ReadMode 
+-- do file <- openFile "/Users/newton/cnc/experimental/graphPartitioner/test2.harch" ReadMode 
+ do file <- openFile "/Users/newton/cnc/experimental/graphPartitioner/outputs/pipes.harch.partitioned" ReadMode 
     txt <- hGetContents file
     let ls = run harchfile txt
     sequence_$ map print ls
+
+
+----------------------------------------------------------------------------------------------------
+-- Conversion to partitioned format:
+
+extractPartitions :: [HarchNode] -> HarchTreeUnordered
+extractPartitions nodes = undefined
+ where
+  foo = map (\ HarchNode{..} -> properties ) nodes
+
 
 ----------------------------------------------------------------------------------------------------
 -- Testing 
@@ -87,6 +139,9 @@ t1 = runPr prop$ "name=foo;"
 t2 = runPr props$ "name=foo; direction=01;" -- No spcs at start end
 t3 = runPr props$ "name=foo;" 
 
+t4 = runPr props$ "name=foo; blah=baz; " 
+
+
 l1 = "% HARCHNODE name=blah; direction=01;\n"
 --t5 = runPr debug$ l1
 --t5 = runPr nodeHeader l1
@@ -98,6 +153,7 @@ t6 = runPr numbers$ l2
 t7 = runPr foo "   \n"
 t8 = runPr foo "\n"
 t9 = runPr bar "\n"
+
 
 
 --t11 = runPr harchnode$ "% HARCHNODE name=blah; direction=01;\nw% HARCHNODE name=blah; direction=01; 0 1 2"
