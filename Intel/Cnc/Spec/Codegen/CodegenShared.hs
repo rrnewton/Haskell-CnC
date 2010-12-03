b561786087f573e63b5bf8161e0474274c5215c1
ryan.newton@alum.mit.edu <unknown>
2010-12-03 14:13:20 +0000

    RN202# This type error is a bit mysterious to me.  It reports a mismatched 'scalar' type when it should report a mismatched function type I think.

--- Intel/Cnc/Spec/Codegen/CodegenShared.hs
+++ Intel/Cnc/Spec/Codegen/CodegenShared.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 
 module Intel.Cnc.Spec.Codegen.CodegenShared where
 
-import Intel.Cnc.EasyEmit 
+import Intel.Cnc.EasyEmit  hiding (app, not, (&&), (==), (||))
 import Intel.Cnc.Spec.Util
 import Intel.Cnc.Spec.CncGraph (ColName) -- hiding (app, not, (&&), (==))
 
@@ -43,19 +44,43 @@ data CodeGenHooks = CodeGenHooks
       -- This predicate determines which steps to which the hooks are applied.
       hooksPredicate :: ColName -> Bool,
 
+      -- Initializes state for a collection.
+      
+
+      -- I vacillated on whether or not to put all of these hooks
+      -- (functions) inside a Maybe.  That would make it more clear
+      -- what a plugin DOESN'T implement, but I think it makes the
+      -- plugins more clunky to use, and makes the belowe combineHooks
+      -- function harder to write.  Besides, there is a sensible
+      -- default hook -- it emits nothing.
+
+
       -- Takes collection name, reference to tag, reference to item.
-      beforeItemPut :: Maybe (ColName -> Syntax -> Syntax -> EasyEmit ()),
-      afterItemPut  :: Maybe (ColName -> Syntax -> Syntax -> EasyEmit ()),
+      -- beforeItemPut :: (ColName -> Syntax -> Syntax -> EasyEmit ()),
+      -- afterItemPut  :: Maybe (ColName -> Syntax -> Syntax -> EasyEmit ()),
 
-      beforeReducerPut :: Maybe (ColName -> Syntax -> Syntax -> EasyEmit ()),
-      afterReducerPut  :: Maybe (ColName -> Syntax -> Syntax -> EasyEmit ()),
+      -- beforeReducerPut :: Maybe (ColName -> Syntax -> Syntax -> EasyEmit ()),
+      -- afterReducerPut  :: Maybe (ColName -> Syntax -> Syntax -> EasyEmit ()),
 
-      beforeTagPut :: Maybe (ColName -> Syntax -> EasyEmit ()),
-      afterTagPut  :: Maybe (ColName -> Syntax -> EasyEmit ()),
+      -- beforeTagPut :: Maybe (ColName -> Syntax -> EasyEmit ()),
+      -- afterTagPut  :: Maybe (ColName -> Syntax -> EasyEmit ()),
+
+      -- -- Takes a reference to the tag
+      -- beforeStepExecute :: Maybe (ColName -> Syntax -> EasyEmit ()),
+      -- afterStepExecute  :: Maybe (ColName -> Syntax -> EasyEmit ())
+
+      beforeItemPut :: (ColName -> Syntax -> Syntax -> EasyEmit ()),
+      afterItemPut  :: (ColName -> Syntax -> Syntax -> EasyEmit ()),
+
+      beforeReducerPut :: (ColName -> Syntax -> Syntax -> EasyEmit ()),
+      afterReducerPut  :: (ColName -> Syntax -> Syntax -> EasyEmit ()),
+
+      beforeTagPut :: (ColName -> Syntax -> EasyEmit ()),
+      afterTagPut  :: (ColName -> Syntax -> EasyEmit ()),
 
       -- Takes a reference to the tag
-      beforeStepExecute :: Maybe (ColName -> Syntax -> EasyEmit ()),
-      afterStepExecute  :: Maybe (ColName -> Syntax -> EasyEmit ())
+      beforeStepExecute :: (ColName -> Syntax -> EasyEmit ()),
+      afterStepExecute  :: (ColName -> Syntax -> EasyEmit ())
 
     }
   deriving Show
@@ -65,10 +90,47 @@ instance Show (a -> b) where
   show _ = "<fun>"
 
 
-defaultCodeGenHooks = CodeGenHooks
+defaultCodeGenHooks = 
+  -- The default hooks just do nothing:
+  let threearg = const$ const$ const$ return ()
+      twoarg   =        const$ const$ return ()
+  in CodeGenHooks
   {
-      hooksPredicate    = const False,
-      beforeItemPut     = Nothing,
+      hooksPredicate    = const False, -- And they apply to nothing.
+      beforeItemPut     = threearg,
+      afterItemPut      = threearg,
+      beforeReducerPut  = threearg,
+      afterReducerPut   = threearg,
+      beforeTagPut      = twoarg,
+      afterTagPut       = twoarg,
+      beforeStepExecute = twoarg,
+      afterStepExecute  = twoarg
+
+      -- afterItemPut      = Nothing,
+      -- beforeReducerPut  = Nothing,
+      -- afterReducerPut   = Nothing,
+      -- beforeTagPut      = Nothing,
+      -- afterTagPut       = Nothing,
+      -- beforeStepExecute = Nothing,
+      -- afterStepExecute  = Nothing
+  }
+
+
+-- Combine plugins that operate on different step collections.
+-- The first plugin has first dibs.
+-- This is probably quite inefficient.
+{-
+combineHooks left right = 
+  let lp = hooksPredicate left 
+      rp = hooksPredicate right 
+      combine project = 
+	  case (project left, project right) of
+	    (Nothing,Nothing) -> Nothing
+	    _ -> Just$ \ name -> if lp name then beforeItemPut left else beforeItemPut right,
+  in CodeGenHooks
+  {
+      hooksPredicate    = \ name -> lp name || rp name,
+      beforeItemPut     = 
       afterItemPut      = Nothing,
       beforeReducerPut  = Nothing,
       afterReducerPut   = Nothing,
@@ -76,6 +138,30 @@ defaultCodeGenHooks = CodeGenHooks
       afterTagPut       = Nothing,
       beforeStepExecute = Nothing,
       afterStepExecute  = Nothing
+  }
+-}
+
+
+combineHooks left right = 
+  let lp = hooksPredicate left 
+      rp = hooksPredicate right 
+      combine project name = 
+	  if lp name then project left else project right
+--	  if lp name then project left name else project right name
+
+      test1 :: (ColName -> Syntax -> Syntax -> EasyEmit ()) = beforeItemPut left
+      test2 = beforeItemPut right
+  in CodeGenHooks
+  {
+      hooksPredicate    = \ name -> lp name || rp name,
+      beforeItemPut     = combine beforeItemPut,
+      afterItemPut      = undefined,
+      beforeReducerPut  = undefined,
+      afterReducerPut   = undefined,
+      beforeTagPut      = undefined,
+      afterTagPut       = undefined,
+      beforeStepExecute = undefined,
+      afterStepExecute  = undefined
   }
 
 
@@ -88,7 +174,8 @@ doneCountingPlugin =
   defaultCodeGenHooks 
   {
     beforeStepExecute = 
-      Just $ \ name tag -> 
+      --Just $ 
+      \ name tag -> 
        do 
           putS "Woo!\n"
           return ()
