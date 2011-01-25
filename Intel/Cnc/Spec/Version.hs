e59f8d6e1bda6149cf14ba1d95847c6a4f4b7675
ryan.newton@alum.mit.edu <unknown>
2011-01-25 20:11:50 +0000

    RN238# Oops, version bump incomplete, fixed

--- Intel/Cnc/Spec/Version.hs
+++ Intel/Cnc/Spec/Version.hs
@@ -1,4 +1,4 @@
 -- WARNING: This file is generated from the .cabal file.
 module Intel.Cnc.Spec.Version where
-version = "0.1.3.105"
-builddate = "2011-01-25 20:10:07.199513 UTC"
+version = "0.1.3.106"
+builddate = "2011-01-25 20:11:46.612593 UTC"
